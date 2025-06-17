1e8e461ddd825be42f1cd9c9e59166ed22c4ee12
Mayank Goley <42241477+mayankgoley@users.noreply.github.com>
2025-06-16 23:02:01 -0500

    Two pointers revision

---
+++ Revision/two_pointer_revision.py
@@ -0,0 +1,17 @@
+def max_area(height):
+    left = 0
+    right = len(height) -1
+    max_area = 0
+    while left < right:
+        width = right - left
+        h = min(height[left], height[right])
+        area = width * h
+        max_area = max(max_area, area)
+        if height[left] < height[right]:
+            left +=1
+        else:
+            right -=1
+    return max_area
+
+height = [1,8,6,2,5,4,8,3,7]
+print(max_area(height))
