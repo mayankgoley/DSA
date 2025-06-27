df8bd88c1f2b751c99c2c57208334936dbc5cc8e
Mayank Goley <42241477+mayankgoley@users.noreply.github.com>
2025-06-27 12:59:32 -0700

    Max Sum Subarray

---
+++ Array/Sliding Window/max_sum_subarray.py
@@ -0,0 +1,14 @@
+def max_sum_subarray(nums, k):
+    n = len(nums)
+    max_sum = current_sum = sum(nums[:k])
+
+    for i in range(k,n):
+        current_sum = current_sum - nums[i-k] + nums[i]
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+nums = [2, 1, 5, 1, 3, 2]
+k = 3
+
+print(max_sum_subarray(nums, k))
